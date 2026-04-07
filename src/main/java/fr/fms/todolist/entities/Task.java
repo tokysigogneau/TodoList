46fab0ce1b83e3af544a8808ae396f429d75dbc2
tokytoky <toky.sigogneau@fms-ea.com>
2026-04-07 08:53:57 +0200

    Added constructor, getter and setter for Task class

--- src/main/java/fr/fms/todolist/entities/Task.java
+++ src/main/java/fr/fms/todolist/entities/Task.java
@@ -6,7 +6,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.io.Serializable;
@@ -35,5 +34,72 @@ public class Task implements Serializable {
     @Size(min=5,max=100)
     private String description;
 
+    @ManyToOne
+    private Category category;
 
+    @ManyToOne
+    private Progression progression;
+
+    public Task(String name, Date date, String description, Category category, Progression progression){
+        this.name = name;
+        this.date = date;
+        this.description = description;
+        this.category = category;
+        this.progression = progression;
+    }
+
+    public String toString(){
+        return " Task {name : "+ name + " date : " + date + " description" + description + "}";
+    }
+
+    //region getter and setter
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public Progression getProgression() {
+        return progression;
+    }
+
+    public void setProgression(Progression progression) {
+        this.progression = progression;
+    }
+
+    //endregion
 }
