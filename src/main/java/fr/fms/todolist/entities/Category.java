fb5c6599ded71ee4537e9fc54327564418b1701b
tokytoky <toky.sigogneau@fms-ea.com>
2026-04-07 09:54:41 +0200

    Updated class Category : added constructor and getter and setters

--- src/main/java/fr/fms/todolist/entities/Category.java
+++ src/main/java/fr/fms/todolist/entities/Category.java
@@ -3,9 +3,11 @@ package fr.fms.todolist.entities;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.io.Serializable;
+import java.util.Collection;
 
 public class Category implements Serializable {
     private static final long serialVersionUID = 1L;
@@ -17,4 +19,37 @@ public class Category implements Serializable {
     @NotNull
     @Size(min = 5, max = 50)
     private String name;
+
+    @OneToMany(mappedBy = "category")
+    private Collection<Task> tasks;
+
+    public Category(){
+
+    }
+    public Category(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Category {name : " + name + " }";
+    }
+
+    //region Getter and setter
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    //endregion
 }
