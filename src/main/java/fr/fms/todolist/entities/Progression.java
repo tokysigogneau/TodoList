ea3ca872505c53040be9c5f5e63ef98165e2ad9e
tokytoky <toky.sigogneau@fms-ea.com>
2026-04-07 08:42:37 +0200

    Created progression.java with main information.

---
+++ src/main/java/fr/fms/todolist/entities/Progression.java
@@ -0,0 +1,21 @@
+package fr.fms.todolist.entities;
+
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+
+public class Progression implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Size(min = 5, max = 50)
+    private String name;
+}
