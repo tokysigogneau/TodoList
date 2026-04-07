b6c61fd29ed6680a5cdc342d55a23e6be5190528
tokytoky <toky.sigogneau@fms-ea.com>
2026-04-07 13:08:47 +0200

    Updated TaskRepository.java : Added 2 querries :  - findByCategoryId()  - findByDescriptionContains()

--- src/main/java/fr/fms/todolist/dao/TaskRepository.java
+++ src/main/java/fr/fms/todolist/dao/TaskRepository.java
@@ -1,7 +1,11 @@
 package fr.fms.todolist.dao;
 
 import fr.fms.todolist.entities.Task;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
+    Page<Task> findByCategoryId(Long id, Pageable pageable);
+    Page<Task> findByDescriptionContains(String description, Pageable pageable);
 }
