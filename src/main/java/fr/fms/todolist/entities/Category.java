2eb005b852ee89c5cde0bf8caddb1bade32288a2
tokytoky <toky.sigogneau@fms-ea.com>
2026-04-07 08:37:11 +0200

    Created category.java with main information.

---
+++ src/main/java/fr/fms/todolist/entities/Category.java
@@ -0,0 +1,20 @@
+package fr.fms.todolist.entities;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+
+public class Category implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Size(min = 5, max = 50)
+    private String name;
+}
