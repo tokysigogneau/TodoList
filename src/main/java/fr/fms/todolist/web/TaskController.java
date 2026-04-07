cae9df60c1d0f25133be3ba1f84d4dcbb0c5694a
tokytoky <toky.sigogneau@fms-ea.com>
2026-04-07 11:35:23 +0200

    Updated TaskController.java  Created function Index to lead to/index page : called home.html

--- src/main/java/fr/fms/todolist/web/TaskController.java
+++ src/main/java/fr/fms/todolist/web/TaskController.java
@@ -1,4 +1,17 @@
 package fr.fms.todolist.web;
 
+import fr.fms.todolist.dao.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class TaskController {
+    @Autowired
+    TaskRepository taskRepository;
+
+    @GetMapping("/index")
+    public String index(){
+        return "home";
+    }
 }
