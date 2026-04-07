d76f220ce8fdbadc778977d7535eedc9ff1982c2
tokytoky <toky.sigogneau@fms-ea.com>
2026-04-07 10:24:49 +0200

    Updated User.java : Added : Getter and setter,  @Entity and @ElementCollection.

--- src/main/java/fr/fms/todolist/entities/User.java
+++ src/main/java/fr/fms/todolist/entities/User.java
@@ -1,12 +1,12 @@
 package fr.fms.todolist.entities;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.io.Serializable;
+import java.util.Collection;
 
+@Entity
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
@@ -17,4 +17,40 @@ public class User implements Serializable {
     @NotNull
     @Size(min = 5, max = 50)
     private String name;
+
+    @OneToMany (mappedBy = "user")
+    @ElementCollection(targetClass = Task.class)
+    private Collection<Task> tasks;
+
+    public User(){
+
+    }
+    public User(String name){
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "User {name : " + name + " }";
+    }
+
+    //region Getter and setter
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    //endregion
 }
