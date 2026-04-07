1cffc196be723a867ff4e170ca552cfd32fe6750
tokytoky <toky.sigogneau@fms-ea.com>
2026-04-07 09:57:45 +0200

    Updated class Progression.java : added constructor and getter and setters

--- src/main/java/fr/fms/todolist/entities/Progression.java
+++ src/main/java/fr/fms/todolist/entities/Progression.java
@@ -4,9 +4,11 @@ package fr.fms.todolist.entities;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.io.Serializable;
+import java.util.Collection;
 
 public class Progression implements Serializable {
     private static final long serialVersionUID = 1L;
@@ -18,4 +20,39 @@ public class Progression implements Serializable {
     @NotNull
     @Size(min = 5, max = 50)
     private String name;
+
+    @OneToMany(mappedBy = "progression")
+    private Collection<Task> tasks;
+
+    public Progression(){
+
+    }
+    public Progression(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Progression {name : " + name + " }";
+    }
+
+    //region Getter and setter
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    //endregion
 }
