161041f75bbb87376d32f3607780a790e8dc1033
tokytoky <toky.sigogneau@fms-ea.com>
2026-04-08 09:59:03 +0200

    Updated TaskRepository.java : Added new query to find element by keyword.

--- src/main/java/fr/fms/todolist/dao/TaskRepository.java
+++ src/main/java/fr/fms/todolist/dao/TaskRepository.java
@@ -2,10 +2,12 @@ package fr.fms.todolist.dao;
 
 import fr.fms.todolist.entities.Task;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
     Page<Task> findByCategoryId(Long id, Pageable pageable);
     Page<Task> findByCategoryIdAndNameContains(Long id, String name, Pageable pageable);
+    Page<Task> findByNameContains(String name, Pageable pageable);
 }
